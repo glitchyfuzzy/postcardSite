a77105733a0e4934ae75c2f3175820373a45a7c7
glitchyfuzzy <saran.wrap.2500@gmail.com>
2024-06-29 17:49:12 +0800

    (insert funny commit message here)

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+function openHiTxt(element) {
+    element.style.display = "none";
+}
 
+function openFolder(element) {
+    element.style.display = "none";
+}
+
+function openMessage(element) {
+    element.style.display = "none";
+}
+
+function openSuprise(element) {
+    element.style.display = "none";
+}
 
