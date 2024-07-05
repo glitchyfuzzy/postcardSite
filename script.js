c7ff0567cd45585e8f15517de6472dc7f0957677
glitchyfuzzy <saran.wrap.2500@gmail.com>
2024-07-05 21:20:49 +0800

    !! BAD CODE ALERT !!

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-function openHiTxt(element) {
+const hiTxtScreen = document.querySelector("#hi-txt-window");
+const hiTxtOpen = document.querySelector("#hi-icon"); 
+const hiTxtClose = document.querySelector("#hi-close");
+const fileSelectScreen = document.querySelector("#file-select")
+const fileSelectOpen = document.querySelector("#folder");
+const fileSelectClose = document.querySelector("#file-select-close");
+const leaveMessageScreen = document.querySelector("#leave-message");
+const leaveMessageOpen = document.querySelector("#message-button");
+const leaveMessageClose = document.querySelector("#leave-message-close");
+const surpriseScreen = document.querySelector("#surprise-window")
+const surpriseScreenOpen = document.querySelector("#surprise-icon");
+const surpriseScreenClose = document.querySelector("#surprise-close");
+
+function closeWindow(element) {
     element.style.display = "none";
 }
 
-function openFolder(element) {
-    element.style.display = "none";
+function openWindow(element) {
+    element.style.display = "block";
 }
+  
+hiTxtOpen.addEventListener("click", function() {
+  openWindow(hiTxtScreen);
+});
 
-function openMessage(element) {
-    element.style.display = "none";
-}
+hiTxtClose.addEventListener("click", function() {
+  closeWindow(hiTxtScreen);
+});
 
-function openSuprise(element) {
-    element.style.display = "none";
-}
+fileSelectOpen.addEventListener("click", function() {
+  openWindow(fileSelectScreen);
+});
 
+fileSelectClose.addEventListener("click", function() {
+  closeWindow(fileSelectScreen);
+});
+
+leaveMessageOpen.addEventListener("click", function() {
+  openWindow(leaveMessageScreen);
+});
+
+leaveMessageClose.addEventListener("click", function() {
+  closeWindow(leaveMessageScreen);
+});
+
+surpriseScreenOpen.addEventListener("click", function() {
+  openWindow(surpriseScreen);
+});
+
+surpriseScreenClose.addEventListener("click", function() {
+  closeWindow(surpriseScreen);
+});
